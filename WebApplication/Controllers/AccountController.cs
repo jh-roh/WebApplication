447f88cafa5f6d2a4ef877a56f98b7f696e9c759
jh-roh <njh8810@naver.com>
2022-07-14 22:37:56 +0900

    init commit

---
+++ WebApplication/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        List<User> users = new List<User>();
+        
+        /// <summary>
+        /// 로그인
+        /// </summary>
+        /// <returns></returns>
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// 회원 가입
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User model)
+        {
+            if(ModelState.IsValid)
+            {
+                users.Add(model);
+
+
+            }
+
+            return RedirectToAction("Index", "Home");
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication/Models/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+
+    //ASP.NET Identiy(Security)
+    //사용자 로그인(인증)
+    //사용자 Role (Admin, User, PowerUser)
+    //SMS 인증, Email 인증
+
+
+    public class User
+    {
+        /// <summary>
+        /// 사용자 번호
+        /// </summary>
+        [Key] //PK 설정
+        public int UserNo { get; set; }
+
+
+        /// <summary>
+        /// 사용자 이름
+        /// </summary>
+        [Required] //Not Null 설정
+        public string UserName { get; set; }
+
+
+        /// <summary>
+        /// 사용자 ID
+        /// </summary>
+        [Required] //Not Null 설정
+        public string  UserID { get; set; }
+
+
+        /// <summary>
+        /// 사용자 비밀번호 
+        /// </summary>
+        [Required] //Not Null 설정
+        public string UserPassword { get; set; }
+    }
+}
